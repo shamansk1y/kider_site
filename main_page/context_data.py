06f5f484d466032868ef2ee77c9312a4f69b2367
Shamansk1y <shamansky1225@gmail.com>
2023-02-26 22:30:52 +0100

    done

--- main_page/context_data.py
+++ main_page/context_data.py
@@ -1,9 +1,21 @@
+"""
+Module containing functions related to obtaining page context.
+
+Functions:
+- get_common_context: gets the common page context used across multiple pages of the site.
+- get_page_context: gets the page context with the current request taken into account.
+"""
+
+
 from .forms import MakeAppointmentForm, SubscriptionForm, ContactUsForm
 from .models import Slider, Team, About, Testimonial, Classes, Facilities, Call, Gallery, Contacts, Schedule, Headlines
 
 
 def get_common_context():
-
+    """
+    Gets the common page context used across multiple pages of the site.
+    :return:
+    """
     return {
         'slider': Slider.objects.filter(is_visible=True),
         'team': Team.objects.all()[:3],
@@ -23,6 +35,19 @@ def get_common_context():
 
 
 def get_page_context(request):
+    """
+    Gets the page context with the current request taken into account.
+
+    Args:
+    - request: HttpRequest object.
+
+    Returns:
+    - tuple of two elements:
+        - dictionary containing data specific to the current request:
+            'user_manager': True if the user is in the 'manager' group, False otherwise.
+            'user_auth': True if the user is authenticated, False otherwise.
+        - dictionary containing the common page context obtained from the get_common_context function.
+    """
     data = {
         'user_manager': request.user.groups.filter(name='manager').exists(),
         'user_auth': request.user.is_authenticated,
